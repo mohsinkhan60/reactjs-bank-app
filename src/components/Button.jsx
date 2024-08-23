209b9118f7b788cb84c09ac1765cef7d9a2fc7c0
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-08-24 01:32:47 +0500

    add Buisness Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/prop-types */
 
 
-export const Button = () => {
+
+export const Button = ({styles}) => {
   return (
-    <div>Button</div>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[50px] ${styles}`}>
+      Get started
+    </button>
   )
 }
 export default Button
